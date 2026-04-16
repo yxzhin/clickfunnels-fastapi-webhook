006fbef793b6b75a72f96cd3c4dcb905d79b652f
yxzhin <yxzhin@gmail.com>
2026-04-16 19:45:46 +0200

    feat(utils): add fastapi app lifespan

---
+++ src/backend/app/utils/lifespan.py
@@ -0,0 +1,26 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+from typing import Any
+
+from fastapi import FastAPI
+
+from .redis_client import RedisClient
+from .structured_logger import StructuredLogger
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
+    """
+    Context manager for controlling the lifespan of the FastAPI app.
+    Initializes and closes resources at the start and the end of the app.
+    """
+    StructuredLogger.setup()
+    try:
+        await RedisClient.init()
+        yield
+    except Exception as e:
+        StructuredLogger.exception("init.error", error=str(e))
+        raise e
+    finally:
+        await app.state.dishka_container.close()
+        await RedisClient.close()
