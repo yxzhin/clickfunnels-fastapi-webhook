e9c2eb95d010fba4b7bc2c1c8af5818fed39a14d
yxzhin <yxzhin@gmail.com>
2026-07-16 21:30:16 +0200

    refactor(utils): refactor init file

--- src/backend/app/utils/__init__.py
+++ src/backend/app/utils/__init__.py
@@ -1,11 +1,16 @@
+from .clickfunnels_utils import ClickFunnelsUtils
+from .enums import LandingPage
 from .error_handler import setup_error_handling
 from .helpers import Helpers
 from .lifespan import lifespan
 from .redis_client import RedisClient
 from .structured_logger import StructuredLogger, start_time_var, trace_id_var
+from .tasks import broker, process_clickfunnels_webhook, send_sms_task
 from .traceid_middleware import TraceIDMiddleware
 
 __all__ = [
+    "ClickFunnelsUtils",
+    "LandingPage",
     "setup_error_handling",
     "Helpers",
     "lifespan",
@@ -13,5 +18,8 @@ __all__ = [
     "StructuredLogger",
     "start_time_var",
     "trace_id_var",
+    "broker",
+    "process_clickfunnels_webhook",
+    "send_sms_task",
     "TraceIDMiddleware",
 ]
