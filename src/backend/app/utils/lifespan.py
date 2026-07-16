f4fe330f6d94549113d045fb74ac5b2c2c235fa2
yxzhin <yxzhin@gmail.com>
2026-07-16 21:20:28 +0200

    feat(utils): setup di in fastapi app lifespan

--- src/backend/app/utils/lifespan.py
+++ src/backend/app/utils/lifespan.py
@@ -2,10 +2,14 @@ from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 from typing import Any
 
+from dishka import make_async_container
+from dishka.integrations.taskiq import setup_dishka
 from fastapi import FastAPI
 
+from ..di.providers import AsyncClientProvider
 from .redis_client import RedisClient
 from .structured_logger import StructuredLogger
+from .tasks import broker
 
 
 @asynccontextmanager
@@ -16,11 +20,15 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, Any]:
     """
     StructuredLogger.setup()
     try:
+        container = make_async_container(
+            AsyncClientProvider(),
+        )
+        setup_dishka(container=container, broker=broker)
         await RedisClient.init()
         yield
     except Exception as e:
         StructuredLogger.exception("init.error", error=str(e))
         raise e
     finally:
-        await app.state.dishka_container.close()
+        await container.close()
         await RedisClient.close()
