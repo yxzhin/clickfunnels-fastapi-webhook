2bbe68501b52c1d466450e08dacc2df124463dc1
yxzhin <yxzhin@gmail.com>
2026-07-16 21:17:08 +0200

    feat(misc): add taskiq worker && scheduler to prod docker compose; general prod docker compose refactor

--- src/backend/entrypoint.prod.sh
+++ src/backend/entrypoint.prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-exec /app/.venv/bin/gunicorn src.backend.app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+exec gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
