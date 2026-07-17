d2f89b3d1c9b35c4d98e66abe591d8c865965c17
yxzhin <yxzhin@gmail.com>
2026-07-17 18:23:17 +0200

    refactor(utils): use async client base url instead of hardcoding it in ClickFunnelsClient

--- src/backend/app/utils/clickfunnels_client.py
+++ src/backend/app/utils/clickfunnels_client.py
@@ -2,10 +2,6 @@ from typing import Any, Self
 
 from httpx import AsyncClient
 
-from ..config import get_config
-
-config = get_config()
-
 
 class ClickFunnelsClient:
     def __init__(self: Self, httpx_client: AsyncClient) -> None:
@@ -17,12 +13,11 @@ class ClickFunnelsClient:
         body: dict[str, Any],
     ) -> None:
         """update contact details w/ given custom attributes"""
-        workspace_id = config.CLICKFUNNELS_WORKSPACE_ID
         if contact_id:
-            url = f"{config.api_base_url}/api/v2/workspaces/{workspace_id}/contacts/{contact_id}"
+            url = f"/contacts/{contact_id}"
             response = await self._httpx_client.put(url, json=body)
         else:
-            url = f"{config.api_base_url}/api/v2/workspaces/{workspace_id}/contacts"
+            url = "/contacts"
             response = await self._httpx_client.post(url, json=body)
 
         response.raise_for_status()
