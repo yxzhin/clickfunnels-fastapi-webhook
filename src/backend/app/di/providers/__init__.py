5ce002d8565929e4c1798e4fad0f3622ae0dc449
yxzhin <yxzhin@gmail.com>
2026-07-17 18:25:38 +0200

    feat(di): implement TwilioClientProvider

--- src/backend/app/di/providers/__init__.py
+++ src/backend/app/di/providers/__init__.py
@@ -1,3 +1,9 @@
 from .async_client_provider import AsyncClientProvider
+from .clickfunnels_client_provider import ClickFunnelsClientProvider
+from .twilio_client_provider import TwilioClientProvider
 
-__all__ = ["AsyncClientProvider"]
+__all__ = [
+    "AsyncClientProvider",
+    "ClickFunnelsClientProvider",
+    "TwilioClientProvider",
+]
