29123471c246c866a39ae77b38f75074ea11ecd3
yxzhin <yxzhin@gmail.com>
2026-07-18 23:19:57 +0200

    refactor(di): move di && taskiq setup into separate file

---
+++ src/backend/app/utils/di_container.py
@@ -0,0 +1,41 @@
+from dishka import make_async_container
+from dishka.integrations.taskiq import setup_dishka
+from taskiq import TaskiqScheduler
+from taskiq_redis import ListQueueBroker, ListRedisScheduleSource
+
+from ..config import get_config
+from .di_providers import (
+    AsyncClientProvider,
+    ClickFunnelsClientProvider,
+    TwilioClientProvider,
+)
+
+config = get_config()
+
+_schedule_source_ready = False
+
+
+async def ensure_schedule_source_ready() -> None:
+    global _schedule_source_ready
+    if not _schedule_source_ready:
+        await schedule_source.startup()
+        _schedule_source_ready = True
+
+
+broker = ListQueueBroker(url=config.REDIS_URL)
+schedule_source = ListRedisScheduleSource(url=config.REDIS_URL)
+scheduler = TaskiqScheduler(broker=broker, sources=[schedule_source])
+
+container = make_async_container(
+    AsyncClientProvider(),
+    ClickFunnelsClientProvider(),
+    TwilioClientProvider(
+        account_sid=config.TWILIO_ACCOUNT_SID,
+        auth_token=config.TWILIO_AUTH_TOKEN,
+        from_number=config.TWILIO_FROM_NUMBER,
+    ),
+)
+
+setup_dishka(container=container, broker=broker)
+
+from . import tasks  # noqa
