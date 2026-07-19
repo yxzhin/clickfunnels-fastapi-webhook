34e44ed5998b0a0748d9d1ada0329f5a3503ddf1
yxzhin <yxzhin@gmail.com>
2026-07-19 19:40:45 +0200

    feat(utils): implement base

---
+++ src/backend/app/utils/db/base.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """
+    Base class for all models
+    All models must be inherited from this class
+    """
+
+    pass
