1466cdb0260e1eab8f3ec6721d47482884557aa2
yxzhin <yxzhin@gmail.com>
2026-07-20 02:42:10 +0200

    feat(misc): alembic upgrade in dev entrypoint

--- src/backend/entrypoint.dev.sh
+++ src/backend/entrypoint.dev.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
 
-exec uv run fastapi dev app/main.py --host 0.0.0.0 --port 8000
+uv run alembic upgrade head
+
+exec uv run fastapi dev src/backend/app/main.py --host 0.0.0.0 --port 8000
