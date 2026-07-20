5a03e7f0e0b1806e4ae00480c3b94e74da3a4a38
yxzhin <yxzhin@gmail.com>
2026-07-20 17:59:11 +0200

    refactor(utils): minor fixes in cf utils

--- src/backend/app/utils/clickfunnels/clickfunnels_utils.py
+++ src/backend/app/utils/clickfunnels/clickfunnels_utils.py
@@ -60,9 +60,9 @@ class ClickFunnelsUtils:
         email = contact.get("email", {})
         phone_number = Helpers.normalize_phone(contact_data.get("phone_number") or "")
         contact_name = contact.get("name", "").split(" ")
-        try:
+        if len(contact_name) > 1:
             first_name, last_name = contact_name[0], contact_name[1]
-        except Exception:
+        else:
             first_name, last_name = (
                 contact_name[0],
                 None,
